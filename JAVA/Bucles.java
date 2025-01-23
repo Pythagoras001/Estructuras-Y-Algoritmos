1ca1f31bec25ee0a461a3222081dbb23aa055871
Thomas Gomez <thomas.gomezr@upb.edu.co>
2025-01-23 07:15:16 -0500

    Estructuras v2

---
+++ JAVA/Bucles.java
@@ -0,0 +1,38 @@
+package JAVA;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Bucles {
+    public static void main(String[] args) {
+        
+        ArrayList<String> nombres = new ArrayList<>(Arrays.asList("pedro", "juan"));
+
+        // De esta manera iteramos sobre los elmentos de una lista
+        for (String string : nombres) {
+            System.out.println(string);
+        }
+
+        // Aqui tenemos un for anidado
+        for (int i = 0; i < 5; i++) {
+            for (int j = i; j < 5; j++) {
+                System.out.println(j);
+                
+            }
+        }
+
+        // Aqui usamos un while para encontrar el primer numero Impar
+        boolean k = true;
+        ArrayList<Integer> numeros = new ArrayList<>(Arrays.asList(2, 4, 6, 5));
+        Integer n = 0;
+
+        while (k) {
+            if (numeros.get(n) % 2 == 1) {
+                System.out.println(numeros.get(n));
+                k = false;
+            }
+            n++;
+        }
+
+    }
+}
---
+++ JAVA/twoPointers.java
@@ -0,0 +1,35 @@
+package JAVA;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class twoPointers {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> numeros = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        Collections.sort(numeros);
+
+        Integer iz = 0;
+        Integer der = numeros.size()-1;
+        Integer target = 5000;
+
+
+        while (iz < der) {
+            Integer ope = numeros.get(iz)+numeros.get(der);
+            if (ope == target) {
+                System.out.println(iz + " " + der);
+                break;
+            } else if (ope < target){
+                iz++;
+            } else{
+                der--;
+            }
+            
+            if (iz == der) {
+                System.out.println("EL CONJUNTO DE NUMERO NO EXISTE");
+            }
+
+        }
+    }
+}
