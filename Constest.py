14311145a7b8459a78d3c74872d66f800245ed16
Thomas Gomez <thomas.gomezr@upb.edu.co>
2025-04-03 07:51:37 -0500

    Estructuras v17

--- Constest.py
+++ Constest.py
@@ -1,50 +1,33 @@
-# Creamos el dfs
-def dfs(ini):
-    recorrido.append(ini)
-    visita[ini] = 1
-    for vecino in conec[ini]:
-        if visita[vecino]:
-            return -1
-        if dfs(vecino) == -1:
-            return -1
-    return 0
 
-for _ in range(int(input())):
-    # Recibimos la entrada
-    n = int(input())
-    est = list(map(int, input().split()))
 
-    conec = [[] for _ in range(n+1)]
-    pas = set()
 
-    for i in range(n-1):
-        conec[i].append(i+1)
-        pas.add(i+1)
+n = int(input())
 
-    for i in range(n):
-        if not est[i]:
-            conec[i].append(n)
-            pas.add(n)
-        else:
-            conec[n].append(i)
-            pas.add(i)
+gustos = []
 
-    # Buscamos un inicio 
-    ini = (((n*(n+1)))//2) - sum(pas)
+vistosH = [0]*n
+vistosM = [0]*n
 
-    if len(pas) == n+1:
-        maxx = 0
-        for i in range(len(conec)):
-            if len(conec[i]) > maxx:
-                ini = i
-                maxx = len(conec[i])
+noEmpa = []
 
-    # Creamos una matriz de visitas
-    visita = [0]*(n+1)
-    recorrido = []
 
-    # Aplicamos el dfs
-    if dfs(ini) == -1:
-        print(-1)
-    else:
-        print(recorrido)
+for _ in range(n):
+    lista = sorted(list(map(int, input().split()))[1:])
+
+    gustos.append(lista)
+
+print(gustos)
+
+for i in range(n):
+    for index in gustos[i]:
+        if not vistosH[index-1]:
+            vistosH[index-1] = 1
+            vistosM[i] = 1
+            break
+    if not vistosM[i]:
+        noEmpa.append(i+1)
+
+if not len(noEmpa):
+    print("OPTIMAL")
+else:
+    pass
